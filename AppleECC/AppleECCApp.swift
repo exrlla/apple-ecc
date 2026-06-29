644591e16daea88cebdc19e22f9b98c4faa01d38
Apple <apple@MacBook-Pro-136.local>
2026-06-29 15:05:05 -0500

    Init

---
+++ AppleECC/AppleECCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleECCApp.swift
+//  AppleECC
+//
+//  Created by Apple on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleECCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleECCTests/AppleECCTests.swift
@@ -0,0 +1,19 @@
+//
+//  AppleECCTests.swift
+//  AppleECCTests
+//
+//  Created by Apple on 6/29/26.
+//
+
+import Testing
+@testable import AppleECC
+
+struct AppleECCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
